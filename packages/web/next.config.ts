a8eed2cb603113a9658f400bc550fd992c419421
Vlad Temian <vladtemian@gmail.com>
2026-01-23 13:35:03 +0200

    fix: explicitly include next compiled files in tracing

--- packages/web/next.config.ts
+++ packages/web/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import { resolve } from "node:path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  outputFileTracingRoot: resolve(__dirname, "../../"),
+  outputFileTracingIncludes: {
+    "/*": ["./node_modules/next/dist/compiled/**/*"],
+  },
 };
 
 export default nextConfig;
